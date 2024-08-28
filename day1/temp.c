bc5af6dbb5fcd4d3a1a4caeb374cb3979c5137af
MIN_ZZL <62577565+wlwl1011@users.noreply.github.com>
2024-08-28 16:15:20 +0900

    Update temp.c

--- day1/temp.c
+++ day1/temp.c
@@ -1,5 +1,4 @@
-root@dq1:/lg_rw/fct_test# python3 test_start_dq1.py  
-Traceback (most recent call last):
-  File "/lg_rw/fct_test/test_start_dq1.py", line 6, in <module>
-    from concurrent.futures import ThreadPoolExecutor, as_completed
-ModuleNotFoundError: No module named 'concurrent'
+Traceback (most recent call last):                                                                               
+  File "/lg_rw/fct_test/test_start_dq1.py", line 216, in <module>                                                
+    with open(usb_log_file_path, 'a') as usb_log_file:                                                           
+FileNotFoundError: [Errno 2] No such file or directory: 'exfat/fct_test_00:50:43:02:fe:01_.log' 
